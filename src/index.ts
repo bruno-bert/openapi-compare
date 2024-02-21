a4036a468105357f18c928e71cab911c66d994e7
De Paula <bdepaula@la.jnj.com@WLLNXPF41LSDR.wks.jnj.com>
2024-02-21 17:14:31 -0300

    change 1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosRequestConfig } from 'axios';
+import axios from 'axios';
 import _ from 'lodash';
 
 interface RouteSpecification {
@@ -10,17 +10,10 @@ interface RouteSpecification {
 
 async function fetchOpenApiSpec(repoUrl: string): Promise<any> {
 
-    const config: AxiosRequestConfig = {
-        auth: {
-            username: process.env.GIT_USERNAME as unknown as string,
-            password: process.env.GIT_PASSWORD as unknown as string
-        }
-    };
 
-    const url = `${repoUrl}/raw/openapi.json?at=refs%2Fheads%2Fmain`
-    console.log(url)
-    const response = await axios.get(url, config);
 
+    const url = `${repoUrl}/main/openapi.json`
+    const response = await axios.get(url);
     return response.data;
 }
 
@@ -58,31 +51,93 @@ async function analyzeRepository(repoUrl: string, mainSpec: any): Promise<{ [rou
 
     const repoSpec = await fetchOpenApiSpec(repoUrl);
 
+    const getResponseBody = (routeSpec: any) => {
+        const responses = routeSpec.responses
+
+        try {
+            return responses['200']?.content['application/json']?.schema || {}
+        } catch (error) {
+            return responses['204']?.content['application/json']?.schema || {}
+        }
+
+
+    }
+
+    const getSecurity = (routeSpec: any) => {
+
+        const security = routeSpec.responses
+
+        if (security) {
+            return routeSpec.security?.map((security: any) => Object.keys(security)) || []
+        }
+
+
+    }
+
+
+    const getAllowedHeaders = (routeSpec: any) => {
+        const parameters = routeSpec.parameters
+        let allowedHeaders = []
+        if (parameters) {
+            const headerParams = parameters.filter((param: any) => param.in === 'header')
+            if (headerParams) {
+                if (headerParams.length > 0) {
+                    allowedHeaders = headerParams.map((param: any) => param.name) || []
+                }
+            }
+
+        }
+
+        return allowedHeaders
+    }
+
+    const getQueryParemeters = (routeSpec: any) => {
+        let queryParams = []
+        const parameters = routeSpec.parameters
+        if (parameters) {
+            queryParams = parameters.map((param: any) => param.name) || []
+        }
+        return queryParams
+
+    }
+
     for (const route in mainSpec.paths) {
         if (mainSpec.paths.hasOwnProperty(route)) {
             const mainRouteSpec = mainSpec.paths[route];
             const repoRouteSpec = repoSpec.paths[route];
 
-            if (repoRouteSpec) {
-                const discrepancies = await compareSpecifications(
-                    {
-                        queryParameters: mainRouteSpec.parameters?.map((param: any) => param.name) || [],
-                        responseBody: mainRouteSpec.responses['200']?.content['application/json']?.schema || {},
-                        allowedHeaders: mainRouteSpec.parameters?.filter((param: any) => param.in === 'header').map((param: any) => param.name) || [],
-                        authenticationMethods: mainRouteSpec.security?.map((security: any) => Object.keys(security)) || []
-                    },
-                    {
-                        queryParameters: repoRouteSpec.parameters?.map((param: any) => param.name) || [],
-                        responseBody: repoRouteSpec.responses['200']?.content['application/json']?.schema || {},
-                        allowedHeaders: repoRouteSpec.parameters?.filter((param: any) => param.in === 'header').map((param: any) => param.name) || [],
-                        authenticationMethods: repoRouteSpec.security?.map((security: any) => Object.keys(security)) || []
-                    }
-                );
+            const methods = ['get', 'post', 'put', 'delete']
 
-                if (discrepancies.length > 0) {
-                    discrepanciesMap[route] = discrepancies;
-                }
+            if (repoRouteSpec) {
+
+                methods.map(async (method: any) => {
+                    const mainRepoMethod = mainRouteSpec[method]
+                    const compareRepoMethod = repoRouteSpec[method]
+
+                    const discrepancies = await compareSpecifications(
+                        {
+                            queryParameters: getQueryParemeters(mainRepoMethod),
+                            responseBody: getResponseBody(mainRepoMethod),
+                            allowedHeaders: getAllowedHeaders(mainRepoMethod),
+                            authenticationMethods: getSecurity(mainRepoMethod)
+                        },
+                        {
+                            queryParameters: getQueryParemeters(compareRepoMethod),
+                            responseBody: getResponseBody(compareRepoMethod),
+                            allowedHeaders: getAllowedHeaders(compareRepoMethod),
+                            authenticationMethods: getSecurity(compareRepoMethod)
+                        }
+                    );
+
+                    if (discrepancies.length > 0) {
+                        discrepanciesMap[route] = discrepancies;
+                    }
+
+
+                })
+
             } else {
+
                 discrepanciesMap[route] = ['Route not found in repository'];
             }
         }
@@ -94,14 +149,17 @@ async function analyzeRepository(repoUrl: string, mainSpec: any): Promise<{ [rou
 async function main() {
     try {
         // Fetch main repository openapi.json
-        const mainSpec = await fetchOpenApiSpec('https://github.com/bruno-bert/openapi-compare');
+        const mainSpec = await fetchOpenApiSpec('https://raw.githubusercontent.com//bruno-bert/openapi-compare');
 
         // List of bitbucket repository URLs
-        const bitbucketRepos = ['https://github.com/bruno-bert/openapi-compare-1',
-            'https://github.com/bruno-bert/openapi-compare-2', /* Add more repo URLs as needed */];
+        const bitbucketRepos = ['https://raw.githubusercontent.com//bruno-bert/openapi-compare-1',
+            'https://raw.githubusercontent.com//bruno-bert/openapi-compare-2', /* Add more repo URLs as needed */];
+
 
         for (const repoUrl of bitbucketRepos) {
             const discrepancies = await analyzeRepository(repoUrl, mainSpec);
+
+
             console.log(`Discrepancies in ${repoUrl}:`, discrepancies);
         }
     } catch (error: any) {
